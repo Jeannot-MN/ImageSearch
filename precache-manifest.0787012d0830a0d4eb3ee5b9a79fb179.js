0177f9fc988d9755a551e7f0da48651193dbb37b
Jeannot-MN <43864167+Jeannot-MN@users.noreply.github.com>
2020-01-22 15:10:07 +0200

    Add files via upload

---
+++ precache-manifest.0787012d0830a0d4eb3ee5b9a79fb179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d3c1d7502cd7a127a7015f78b9a203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d84a35a44f33b69ed22",
+    "url": "/static/css/main.5d8d154e.chunk.css"
+  },
+  {
+    "revision": "0d49921d992dddbcaed0",
+    "url": "/static/js/2.2cf82f74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2cf82f74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d84a35a44f33b69ed22",
+    "url": "/static/js/main.50902668.chunk.js"
+  },
+  {
+    "revision": "a768eb26d677eec38cfd",
+    "url": "/static/js/runtime-main.85e4a04e.js"
+  }
+]);
